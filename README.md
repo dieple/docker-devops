cdfac4d55ff65dbf574ef4a929f0c501b050cb13
Diep Le <diep.le@sainsburys.co.uk>
2015-10-08 20:42:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# docker-devops devlopment env
+The present docker is an extension of the Jhipster docker, including a MySql database, Grunt, Bower, and a bash script which creates a database and runs the application using the production profile.
---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start mysql service and create a database called devops
+service mysql start
+MYSQL=`which mysql`
+$MYSQL -u root -e "CREATE DATABASE IF NOT EXISTS devops"
+
+# Run your project in production mode.
+# You need to run it as a devops user because 
+# some of the tools are not meant to be run by the root user
+cd /apps
+sudo -u devops -H sh -c "mvn -Pprod spring-boot:run"  
+
+
+
